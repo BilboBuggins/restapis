81740328bd1fe496dbbf33eb5fc4ac1d896c3ec7
sjakula <sumanthjaideep@gmail.com>
2023-04-18 00:51:07 +0200

    Post and Delete operation added

--- src/test/java/feature/restapi.feature
+++ src/test/java/feature/restapi.feature
@@ -1,21 +1,40 @@
 Feature: Test the Rest APIs
 
   Scenario: Test the Get operation for Users
-    Given I send the URL "https://gorest.co.in"
-    When I send the resources as "public/v2/users"
-    Then I expect a response as 200
+    Given User send the URL "https://gorest.co.in"
+    When User send the resources as "public/v2/users" with "Get" call
+    Then User expect a response as 200
 
   Scenario: Test the Get operation for Posts
-    Given I send the URL "https://gorest.co.in"
-    When I send the resources as "public/v2/posts"
-    Then I expect a response as 200
+    Given User send the URL "https://gorest.co.in"
+    When User send the resources as "public/v2/posts" with "Get" call
+    Then User expect a response as 200
 
   Scenario: Test the Get operation for Comments
-    Given I send the URL "https://gorest.co.in"
-    When I send the resources as "public/v2/comments"
-    Then I expect a response as 200
+    Given User send the URL "https://gorest.co.in"
+    When User send the resources as "public/v2/comments" with "Get" call
+    Then User expect a response as 200
 
   Scenario: Test the Get operation for TODO
-    Given I send the URL "https://gorest.co.in"
-    When I send the resources as "public/v2/todos"
-    Then I expect a response as 200
+    Given User send the URL "https://gorest.co.in"
+    When User send the resources as "public/v2/todos" with "Get" call
+    Then User expect a response as 200
+
+  Scenario: Test the Post operation
+    Given User send the URL "https://gorest.co.in"
+    When User send the headers as Key Value pair
+    |  Key                  |  Value                           |
+    |  Content-Type         |  application/json                |
+    |  Authorization        |  Bearer 235e225d46a4dfcdc38beab23d582f83981ac2b46fe6cbce38cbf42d3717d6cd             |
+    And User adds the "postRequest.json" body to the request
+    And User send the resources as "public/v2/users" with "Post" call
+    Then User expect a response as 201
+
+  Scenario: Test the Delete Operation
+    Given User send the URL "https://gorest.co.in"
+    When User send the headers as Key Value pair
+      |  Key                  |  Value                           |
+      |  Content-Type         |  application/json                |
+      |  Authorization        |  Bearer 235e225d46a4dfcdc38beab23d582f83981ac2b46fe6cbce38cbf42d3717d6cd             |
+    And User send the resources as "public/v2/users/1036917" with "Delete" call
+    Then User expect a response as 204
--- src/test/java/stepDefinition/restAPIStepDefinition.java
+++ src/test/java/stepDefinition/restAPIStepDefinition.java
@@ -6,6 +6,10 @@ import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
@@ -16,30 +20,81 @@ public class restAPIStepDefinition {
     private RequestSpecification requestSpecification;
     private Response response;
 
-    @Given("I send the URL {string}")
-    public void i_send_the_url(String string) {
+    /**
+     * This method take a String as the URL and constructs a Request Specification
+     * @param string
+     */
+    @Given("User send the URL {string}")
+    public void user_send_the_url(String string) {
         requestSpecification=given().log().all().baseUri(string);
     }
-    @When("I send the parameters as Key Value pair")
-    public void i_send_the_parameters_as_key_value_pair(io.cucumber.datatable.DataTable dataTable) {
+
+    /**
+     * This method take data in a data table format with key value pair and then converts the data table into
+     * a List of Sting map. It later iterates using a for loop and adds the data as headers to the existing
+     * Request specification.
+     * @param dataTable
+     */
+    @When("User send the parameters as Key Value pair")
+    public void user_send_the_parameters_as_key_value_pair(io.cucumber.datatable.DataTable dataTable) {
         List<Map<String, String>>data=dataTable.asMaps(String.class, String.class);
         for(Map<String,String> pairs:data){
-            requestSpecification=requestSpecification.log().all().headers(pairs.get("Key"), pairs.get("Value"));
+            requestSpecification=requestSpecification.queryParams(pairs.get("Key"), pairs.get("Value"));
         }
     }
-    @When("I send the headers as Key Value pair")
-    public void i_send_the_headers_as_key_value_pair(io.cucumber.datatable.DataTable dataTable) {
+
+    /**
+     * This method take data in a data table format with key value pair and then converts the data table into
+     * a List of Sting map. It later iterates using a for loop and adds the data as query parameters to the existing
+     * Request specification.
+     * @param dataTable
+     */
+    @When("User send the headers as Key Value pair")
+    public void user_send_the_headers_as_key_value_pair(io.cucumber.datatable.DataTable dataTable) {
         List<Map<String, String>>data=dataTable.asMaps(String.class, String.class);
         for(Map<String, String> pair:data){
-            requestSpecification=requestSpecification.log().all().queryParams(pair.get("Key"), pair.get("Value"));
+            requestSpecification=requestSpecification.headers(pair.get("Key"), pair.get("Value"));
         }
     }
-    @When("I send the resources as {string}")
-    public void i_send_the_resources_as(String resources) {
-        response=requestSpecification.log().all().when().get(resources);
+
+    @When("User adds the {string} body to the request")
+    public void user_adds_the_body_to_the_request(String body) throws IOException {
+        requestSpecification=requestSpecification.body(new String(Files.readAllBytes(Paths.get("src/main/java/requestBody/"+body))));
     }
-    @Then("I expect a response as {int}")
-    public void i_expect_a_response_as(int statusCode) {
+
+    /**
+     * This method takes Resources and the call as String Parameters and makes the corresponding call along
+     * with the resources and adds it to the existing Request Specification and returns a response
+     * @param resources
+     * @param call
+     */
+    @When("User send the resources as {string} with {string} call")
+    public void user_send_the_resources_as(String resources, String call) {
+        if(call.equalsIgnoreCase("get")){
+            response=requestSpecification.log().all().when().get(resources);
+        }
+        else if(call.equalsIgnoreCase("post")){
+            response=requestSpecification.log().all().when().post(resources);
+        }
+        else if(call.equalsIgnoreCase("put")){
+            response=requestSpecification.log().all().when().put(resources);
+        }
+        else if(call.equalsIgnoreCase("patch")){
+            response=requestSpecification.log().all().when().patch(resources);
+        }
+        else if(call.equalsIgnoreCase("delete")){
+            response=requestSpecification.log().all().when().delete(resources);
+        }
+
+    }
+
+    /**
+     * This method take the expected status code as an int parameter and checks it against the code of the
+     * response.
+     * @param statusCode
+     */
+    @Then("User expect a response as {int}")
+    public void user_expect_a_response_as(int statusCode) {
         response=response.then().statusCode(statusCode).log().all().extract().response();
     }
 
